71ad051ed61caaa049765eb295211d31f11fbb98
JuneyBoy <arjun.ganesan@ou.edu>
2022-04-08 12:05:41 -0500

    Sorted all the data files alphabetically by team

--- ParseData.py
+++ ParseData.py
@@ -8,6 +8,16 @@ def parse_csv(file_name):
     with open(file_name, "r") as file:
         reader = csv.reader(file)
         for row in reader:
-            all_rows.append(row)
+            all_rows.append(row[2:])
 
-    titles_row = all_rows.pop(0)
+    all_rows.pop(0)
+
+    return all_rows
+
+
+def parse_defensive_advanced():
+    all_rows = parse_csv("Raw Data/DefensiveAdvanced.csv")
+
+    defensive_advanced_data = [float(row[15].strip("%")) / 100 for row in all_rows]
+
+    print(defensive_advanced_data)
