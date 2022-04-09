ac3a6280010bad79935b8ae96641adc99aa23d96
JuneyBoy <arjun.ganesan@ou.edu>
2022-04-09 15:27:20 -0500

    Added knn evaluation; very large errors

--- KNearestNeighbors.py
+++ KNearestNeighbors.py
@@ -1,5 +1,6 @@
 import numpy as np
 from ParseData import training_examples
+from ParseData import validation_examples
 
 
 def euclidean_distance(p1, p2):
@@ -20,11 +21,11 @@ def k_nearest_neighbors_real_value(k, point_to_classify, training_examples, weig
 
     # get distances between each of the training examples and point_to_classify
     for example in training_examples:
-        distances[example] = euclidean_distance(point_to_classify, example)
+        distances[tuple(example)] = euclidean_distance(point_to_classify, example)
 
     # sorts dictionary by distances from least to greatest
     sorted_distances = dict(sorted(distances.items(), key=lambda item: item[1]))
-    # print(sorted_distances)
+    print(sorted_distances)
     # keeps track of the sum of the outputs for the k-nearest neighbors
     sum = 0
     # keeps track of the weights of the k-nearest neighbors
@@ -44,3 +45,22 @@ def k_nearest_neighbors_real_value(k, point_to_classify, training_examples, weig
         return sum / np.sum(ws)
     else:
         return sum / k
+
+
+k_nearest_neighbors_real_value(
+    3, (validation_examples[0])[:-1], training_examples, True
+)
+
+# predicted_wins = [
+#     k_nearest_neighbors_real_value(3, team_data[:-1], training_examples, True)
+#     for team_data in validation_examples
+# ]
+
+# print(
+#     [
+#         validation_example[-1] - predicted_win
+#         for validation_example, predicted_win in zip(
+#             validation_examples, predicted_wins
+#         )
+#     ]
+# )
--- ParseData.py
+++ ParseData.py
@@ -1,13 +1,6 @@
 import csv, operator
 
 
-def get_wins(file_name):
-    wins = csv.reader(open(file_name), delimiter=",")
-    wins = sorted(wins, key=operator.itemgetter(0))
-
-    return [win[1] for win in wins]
-
-
 def parse_csv(file_name):
 
     all_rows = []
@@ -22,68 +15,93 @@ def parse_csv(file_name):
     return all_rows
 
 
-def parse_defensive_advanced(stats, i, file_name):
+def parse_defensive_advanced(stats, file_name):
     all_rows = parse_csv(file_name)
-    # Quarterback (QB) pressures per drop back
-    stats[i] = [float(row[14].strip("%")) / 100 for row in all_rows]
+
+    for i, row in enumerate(all_rows):
+        # Quarterback (QB) pressures per drop back
+        # print(str(i))
+        stats[i][0] = float(row[14].strip("%")) / 100
 
 
-def parse_defensive_drives(stats, i, file_name):
+def parse_defensive_drives(stats, file_name):
     all_rows = parse_csv(file_name)
-    # percentage of offensive drives ending in score
-    stats[i] = [float(row[2].strip("%")) / 100 for row in all_rows]
-    # percentage of offensive drives ending in turnover
-    stats[i + 1] = [float(row[3].strip("%")) / 100 for row in all_rows]
+
+    for i, row in enumerate(all_rows):
+        # percentage of offensive drives ending in score
+        stats[i][1] = float(row[2].strip("%")) / 100
+        # percentage of offensive drives ending in turnover
+        stats[i][2] = float(row[3].strip("%")) / 100
 
 
-def parse_defensive_passing(stats, i, file_name):
+def parse_defensive_passing(stats, file_name):
     all_rows = parse_csv(file_name)
-    # percentage of attempted passes by offensive team intercepted
-    stats[i] = [float(row[8].strip("%")) / 100 for row in all_rows]
+
+    for i, row in enumerate(all_rows):
+        # percentage of attempted passes by offensive team intercepted
+        stats[i][3] = float(row[8].strip("%")) / 100
 
 
-def parse_offensive_drives(stats, i, file_name):
+def parse_offensive_drives(stats, file_name):
     all_rows = parse_csv(file_name)
-    # percentage of offensive drives ending in score
-    stats[i] = [float(row[2].strip("%")) / 100 for row in all_rows]
-    # percentage of offensive drives ending in turnover
-    stats[i + 1] = [float(row[3].strip("%")) / 100 for row in all_rows]
-    # avg number of points scored per drive
-    stats[i + 2] = [float(row[8]) for row in all_rows]
+
+    for i, row in enumerate(all_rows):
+        # percentage of offensive drives ending in score
+        stats[i][4] = float(row[2].strip("%")) / 100
+        # percentage of offensive drives ending in turnover
+        stats[i][5] = float(row[3].strip("%")) / 100
+        # avg number of points scored per drive
+        stats[i][6] = float(row[8]) / 100
 
 
-def parse_offensive_passing(stats, i, file_name):
+def parse_offensive_passing(stats, file_name):
     all_rows = parse_csv(file_name)
-    # pass completion
-    stats[i] = [float(row[2].strip("%")) / 100 for row in all_rows]
-    # adjusted yards gained per pass attempt
-    stats[i + 1] = [float(row[10]) for row in all_rows]
+
+    for i, row in enumerate(all_rows):
+        # pass completion
+        stats[i][7] = float(row[2].strip("%")) / 100
+        # adjusted yards gained per pass attempt
+        stats[i][8] = float(row[10])
 
 
-def parse_offensive_rushing(stats, i, file_name):
+def parse_offensive_rushing(stats, file_name):
     all_rows = parse_csv(file_name)
-    # percentage of attempted passes by offensive team intercepted
-    stats[i] = [float(row[4]) for row in all_rows]
+
+    for i, row in enumerate(all_rows):
+        # yards gained per attempt
+        stats[i][9] = float(row[4])
 
 
-training_examples = [0] * 11
+def get_wins(stats, file_name):
+    wins = csv.reader(open(file_name), delimiter=",")
+    # sorts each row by the Team Name
+    wins = sorted(wins, key=operator.itemgetter(0))
+    # gets rid of label row (which ends up being the 2nd to last row after sorting)
+    wins.pop(-2)
+
+    for i, row in enumerate(wins):
+        stats[i][10] = float(row[1])
+
+
+training_examples = [[0] * 11 for i in range(32)]
+parse_defensive_advanced(training_examples, "Raw Data/2021_Data/DefensiveAdvanced.csv")
+parse_defensive_drives(training_examples, "Raw Data/2021_Data/DefensiveDrives.csv")
+parse_defensive_passing(training_examples, "Raw Data/2021_Data/DefensivePassing.csv")
+parse_offensive_drives(training_examples, "Raw Data/2021_Data/DefensivePassing.csv")
+parse_offensive_passing(training_examples, "Raw Data/2021_Data/OffensivePassing.csv")
+parse_offensive_rushing(training_examples, "Raw Data/2021_Data/OffensiveRushing.csv")
+get_wins(training_examples, "Raw Data/2021_Data/2021_Standings.csv")
+
+validation_examples = [[0] * 11 for i in range(32)]
 parse_defensive_advanced(
-    training_examples, 0, "Raw Data/2021_Data/DefensiveAdvanced.csv"
+    validation_examples, "Raw Data/2020_Data/DefensiveAdvanced.csv"
 )
-parse_defensive_drives(training_examples, 1, "Raw Data/2021_Data/DefensiveDrives.csv")
-parse_defensive_passing(training_examples, 3, "Raw Data/2021_Data/DefensivePassing.csv")
-parse_offensive_drives(training_examples, 4, "Raw Data/2021_Data/DefensivePassing.csv")
-parse_offensive_passing(training_examples, 7, "Raw Data/2021_Data/OffensivePassing.csv")
-parse_offensive_rushing(training_examples, 9, "Raw Data/2021_Data/OffensiveRushing.csv")
-training_examples[10] = get_wins("Raw Data/2021_Data/2021_Data/2021_Standings.csv")
+parse_defensive_drives(validation_examples, "Raw Data/2020_Data/DefensiveDrives.csv")
+parse_defensive_passing(validation_examples, "Raw Data/2020_Data/DefensivePassing.csv")
+parse_offensive_drives(validation_examples, "Raw Data/2020_Data/DefensivePassing.csv")
+parse_offensive_passing(validation_examples, "Raw Data/2020_Data/OffensivePassing.csv")
+parse_offensive_rushing(validation_examples, "Raw Data/2020_Data/OffensiveRushing.csv")
+get_wins(validation_examples, "Raw Data/2020_Data/2020_Standings.csv")
 
-validation_examples = [0] * 11
-parse_defensive_advanced(
-    training_examples, 0, "Raw Data/2020_Data/DefensiveAdvanced.csv"
-)
-parse_defensive_drives(training_examples, 1, "Raw Data/2020_Data/DefensiveDrives.csv")
-parse_defensive_passing(training_examples, 3, "Raw Data/2020_Data/DefensivePassing.csv")
-parse_offensive_drives(training_examples, 4, "Raw Data/2020_Data/DefensivePassing.csv")
-parse_offensive_passing(training_examples, 7, "Raw Data/2020_Data/OffensivePassing.csv")
-parse_offensive_rushing(training_examples, 9, "Raw Data/2020_Data/OffensiveRushing.csv")
-validation_examples[10] = get_wins("Raw Data/2020_Data/2021_Data/2021_Standings.csv")
+# print(training_examples)
+print(validation_examples[0])
